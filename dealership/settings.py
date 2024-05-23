9b14f2db69a63d9a5d517ca318469fa9ecb06df8
Sheela-cla <sap.sheela@gmail.com>
2024-05-23 12:17:46 +0200

    settings bug fixed

--- dealership/settings.py
+++ dealership/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-
+#DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG =True
 ALLOWED_HOSTS = []
 
 
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'dealership.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
