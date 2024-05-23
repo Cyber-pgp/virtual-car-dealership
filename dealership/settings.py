bc50b1ff66e06d1661a23d21eccddbbb55172ec0
Sheela-cla <sap.sheela@gmail.com>
2024-05-23 11:40:04 +0200

    env seceret key added

--- dealership/settings.py
+++ dealership/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-#from decouple import config
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
