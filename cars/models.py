e827da196187dbcb77f134c074e0b4bb2436d4aa
Sheela-cla <sap.sheela@gmail.com>
2024-05-23 11:31:48 +0200

    model changes done

--- cars/models.py
+++ cars/models.py
@@ -1,30 +1,6 @@
 from django.db import models
-
-
-
 # Create your models here.
-<<<<<<< HEAD
 
-from django.db import models
-
-class Car(models.Model):
-    
-    make = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    year = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='car_images/', blank=True, null=True)
-    miles = models.IntegerField()
-    passengers = models.IntegerField()
-    no_of_owners = models.CharField(max_length=100)
-    milage = models.IntegerField()
-    fuel_type = models.CharField(max_length=50)
-    color = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.year} {self.make} {self.model} {self.color} {self.fuel_type}"
-=======
 class Car(models.Model):
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
@@ -36,4 +12,4 @@ class Car(models.Model):
     
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
->>>>>>> fa05d3787ffd17b5b3604c69c85c4db970af360f
+
