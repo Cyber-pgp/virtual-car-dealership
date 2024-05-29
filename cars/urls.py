f2e7403bca2c8d1a7381b71f0ceef8679929104a
Cyber-pgp <dean@118110.se>
2024-05-29 12:49:54 +0200

    m

--- cars/urls.py
+++ cars/urls.py
@@ -10,14 +10,4 @@ urlpatterns = [
     path('add/', views.CarCreateView.as_view(), name='car_add'),
     path('<int:pk>/edit/', views.CarUpdateView.as_view(), name='car_edit'),
     path('<int:pk>/delete/', views.CarDeleteView.as_view(), name='car_delete'),
-
-
-    # This is all path details of home,list,details,search
-    path('', views.home, name='home'),
-    path('list/', views.listings, name='listings'),
-    path('cars/<int:car_id>/', views.car_details, name='car_details'),
-    path('search/', views.search, name='search'),
-
-
-
 ]
