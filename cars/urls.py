7eb96bc293c9c113f1ffb3fdb23c5a03005b8ba8
Cyber-pgp <dean@118110.se>
2024-05-31 11:52:42 +0200

    working with login

--- cars/urls.py
+++ cars/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     path('listings/', views.listings, name='listings'),  # Changed from 'list' to 'listings'
     path('cars<int:car_id>/', views.car_details, name='car_details'),  # Removed redundant 'cars' prefix
     path('search/', views.search, name='search'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('admindashboard/',views.admindashboard,name='admindashboard'),
+    path('adminlogin/',views.adminlogin,name='adminlogin'),
+    path('adminlogout/',views.adminlogout,name='adminlogout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,11 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Car
 from django.urls import reverse_lazy
 from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def home(request):
@@ -30,3 +35,92 @@ def search(request):
     else:
         cars = []
     return render(request, 'cars/search.html', {'cars': cars, 'query': query})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+ 
+        if user is not None:
+            auth.login(request, user)
+            #messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request,'Oops! Invalid login credentials')
+            return redirect('login')
+    return render(request, 'login.html')
+ 
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    return redirect('home')
+ 
+def register(request):
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+ 
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists!')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists!')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=firstname, last_name=lastname, email=email, username=username, password=password)
+                    auth.login(request, user)
+                    user.save()
+                    messages.success(request, 'You are registered successfully.')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password does not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+   
+def dashboard(request):
+    return render(request,'dashboard.html')
+ 
+ 
+ 
+def admindashboard(request):
+    num_users = User.objects.exclude(is_superuser=True).count()
+    context = {'num_users': num_users}
+    return render(request, 'admindashboard.html', context)
+ 
+ 
+ 
+ 
+def adminlogin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request)
+                return redirect('admindashboard')
+            else:
+                messages.error(request, 'you are not ad admin')
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'adminlogin.html', {'form': form})
+ 
+def adminlogout(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+    return redirect('home')
+ 
